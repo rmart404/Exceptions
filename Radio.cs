aab30537a7db354e37246a3c42c95d33e6810562
Ray Martinez <rmart404@stu.southtexascollege.edu>
2021-06-16 11:37:39 -0500

    Week 2 Done

---
+++ Radio.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SimpleException
+{
+    class Radio
+    {
+        public void TurnOn(bool on)
+        {
+            Console.WriteLine(on ? "Jamming..." : "Quiet time...");
+        }
+    }
+}
